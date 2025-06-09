0da5263692827fd33355114b57f2526b9a202c13
Muhammad Othman Ghani <67419729+mothman2503@users.noreply.github.com>
2025-06-09 21:53:55 +0000

    Sports Inference test modified

--- sports.py
+++ sports.py
@@ -3,4 +3,4 @@ import pandas as pd
 df = pd.read_csv("final_balanced_multilingual_dataset.csv")
 sports_df = df[df['iab_label'] == "IAB17 Sports"]
 
-print(sports_df.sample(10, random_state=42)[["search_term", "synthetic"]])
+print(sports_df.sample(10, random_state=42)[["query_or_title", "source"]])
