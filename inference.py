d5308df077d3100c8cb4b30f693d3c824854be95
Muhammad Othman Ghani <67419729+mothman2503@users.noreply.github.com>
2025-06-09 21:59:23 +0000

    Inference test modified

--- inference.py
+++ inference.py
@@ -10,7 +10,7 @@ tokenizer = BertTokenizerFast.from_pretrained(model_path)
 model.eval()
 
 # Your test query
-query = "pizza"
+query = "jobs in canada"
 
 # Tokenize
 inputs = tokenizer(query, return_tensors="pt", truncation=True, padding=True)
